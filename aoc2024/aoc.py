c31095e881cd2c55c74428b8a4c6a7302ef0d92f
UserNombre <pabloestebanbaquero@gmail.com>
2024-12-21 11:29:44 +0100

    [IMP] aoc2024/aoc.py: refactor function interface + various improvements

--- aoc2024/aoc.py
+++ aoc2024/aoc.py
@@ -1,67 +1,70 @@
 #!/usr/bin/env python3
 
-import sys
-import argparse
-import importlib
 from pathlib import Path
 
-def run(args):
-    if args.filename:
-        path = Path(args.filename)
+def run(module):
+    if module.input_path:
+        input_path = Path(module.input_path)
     else:
-        path = Path(args.path).with_suffix(".txt")
-    string_input = path.read_text()
-    result = aoc.solve(string_input)
+        input_path = Path(module.__file__).with_suffix(".txt")
+    string_input = input_path.read_text()
+    result = module.solve(string_input)
     print(result)
-    if args.interactive:
-        shell(locals())
+    if module.interact:
+        shell(module, locals())
 
-def check(args):
-    result = aoc.solve(aoc.sample_input)
-    if result == aoc.sample_result:
+def check(module):
+    result = module.solve(module.sample_input)
+    if result == module.sample_result:
         print("Check succeeded")
-        print(f"{result} == {aoc.sample_result}")
+        print(f"{result} == {module.sample_result}")
     else:
         print("Check failed")
-        print(f"{result} != {aoc.sample_result}")
-    if args.interactive:
-        shell(locals())
+        print(f"{result} != {module.sample_result}")
+    if module.interact:
+        shell(module, locals())
 
-def shell(variables):
+def shell(module, variables={}):
     import code
     import readline
     import rlcompleter
-    readline.parse_and_bind("tab: complete")
-    code.interact(local=dict(**globals(), **locals(), **variables))
 
-def main():
+    variables.update(vars(module))
+    readline.set_completer(rlcompleter.Completer(variables).complete)
+    readline.parse_and_bind("tab: complete")
+    code.interact(local=variables)
+
+def main(command, module_path, input_path=None, debug=False, interact=False):
+    import importlib
+
+    module_name = str(Path(module_path).with_suffix(""))
+    module = importlib.import_module(module_name)
+    module.input_path = input_path
+    module.debug = debug
+    module.interact = interact
+
+    if command in ["run", "check", "shell"]:
+        module.mode = command
+        globals()[command](module)
+    else:
+        raise UserWarning(f"command {command} does not exist")
+
+def cli_main():
+    import sys
+    import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("command")
-    parser.add_argument("path")
+    parser.add_argument("module_path")
+    parser.add_argument("-p", "--input-path")
     parser.add_argument("-d", "--debug", action="store_true")
-    parser.add_argument("-i", "--interactive", action="store_true")
-    parser.add_argument("-f", "--filename")
+    parser.add_argument("-i", "--interact", action="store_true")
     args = parser.parse_args()
 
-    module_name = str(Path(args.path).with_suffix(""))
-    globals()["aoc"] = importlib.import_module(module_name)
-
-    aoc.debug = args.debug
-    if args.command == "run":
-        aoc.mode = "run"
-        run(args)
-    elif args.command == "check":
-        aoc.mode = "check"
-        check(args)
-    elif args.command == "shell":
-        aoc.mode = "shell"
-        shell(locals())
-    else:
-        raise UserWarning(f"command {args.command} does not exist")
-
-if __name__ == "__main__":
     try:
-        main()
+        if args.debug:
+            sys.stdout.write("\033[?25l")
+        main(args.command, args.module_path, args.input_path, args.debug, args.interact)
     except KeyboardInterrupt:
         pass
     except FileNotFoundError as e:
@@ -73,5 +76,12 @@ if __name__ == "__main__":
     except Exception as e:
         import traceback
         print(traceback.format_exc())
+        if args.debug:
+            import pdb
+            sys.stdout.write("\033[?25h")
+            pdb.post_mortem()
     finally:
         sys.stdout.write("\033[?25h")
+
+if __name__ == "__main__":
+    cli_main()
